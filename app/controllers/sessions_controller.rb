ca66090d2c3ccdf8a2eb8e4532bb5f0efdf39d5e
kouphasi <phasi.ok.phasi@gmail.com>
2022-10-29 16:26:40 +0900

    説明の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
 
   def create
     account=AccountTable.find_by(id: params[:session][:id])
-    # puts account
+    # hash値エラーが出るのは登録するときに、password_digestに直接入れているから
+    # passwordとして登録すれば、勝手にpassword_digestに入る
     if account && account.authenticate(params[:session][:password_digest])
       log_in account
       redirect_to root_url
