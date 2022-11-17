a7440b1766d4c5e6fec8ddd383162d3e1436b93b
kouphasi <phasi.ok.phasi@gmail.com>
2022-11-17 18:56:43 +0900

    add follow function

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,10 @@
 class RelationshipsController < ApplicationController
+    def create
+        current_user.follow(params[:account_table_id])
+        redirect_to request.referer
+    end
+    def destroy
+        current_user.unfollow(params[:account_table_id])
+        redirect_to request.referer
+    end
 end
