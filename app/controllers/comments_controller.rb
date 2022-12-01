adc131d9839846f5cefbec549509d02131e65e4f
kouphasi <phasi.ok.phasi@gmail.com>
2022-12-01 18:58:57 +0900

    コメント機能の完成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
     def create
+        comment = params[:session][:comment]
         tweet = Tweet.find(params[:tweet_id])
-        comment = tweet.comments.build(comment_params)
-        comment.user_id = current_user.id
+        # comment = tweet.comments.build(comment_params)
+        
+        comment = Comment.create(content: comment, account_table_id: current_user.id, tweet_id: tweet.id,  )
 
         if comment.save
-            redirect_back(fallback_location: root_path)
+            redirect_to request.referer
         end
     end
 end
