b21173677494df52d5e1a4e11b9442369741bc5a
kouphasi <phasi.ok.phasi@gmail.com>
2022-12-08 17:46:04 +0900

    不要な行の削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
     def create
         comment = params[:session][:comment]
         tweet = Tweet.find(params[:tweet_id])
-        # comment = tweet.comments.build(comment_params)
         
         comment = Comment.create(content: comment, user_id: current_user.id, tweet_id: tweet.id,  )
 
